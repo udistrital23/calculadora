059edcbc73acf25875220e9d4acee0b728004cff
Matsy - Maximiliano Valeta Medina <mvaleta@altocolombia.com.co>
2025-10-30 22:54:45 -0500

    report

--- src/test/java/com/example/RunCucumberTest.java
+++ src/test/java/com/example/RunCucumberTest.java
@@ -10,6 +10,6 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty,html:target/cucumber-report.html,json:target/cucumber.json")
 public class RunCucumberTest {
 }
